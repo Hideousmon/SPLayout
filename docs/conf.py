3d4b792af1b3b6f309ff4e02eac553576d96e50e
Hideousmon <mailtozyzhao@163.com>
2021-06-29 22:06:56 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -14,18 +14,13 @@
 
 import sys
 import os
-import shlex
-import distutils.command.build
-from distutils.dist import Distribution
+
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
-b = distutils.command.build.build(Distribution())
-b.initialize_options()
-b.finalize_options()
-sys.path.insert(0, os.path.abspath(".." + os.sep + b.build_platlib))
+sys.path.insert(0, os.path.abspath('.'))
 
 # -- General configuration ------------------------------------------------
 
