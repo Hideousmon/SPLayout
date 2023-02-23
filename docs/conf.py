cf02cd4bac50c6171e2da2bdef3da6635d43370f
ZhenyuZhao <mailtozyzhao@163.com>
2023-02-23 23:20:45 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -286,3 +286,12 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
+
+def skip(app, what, name, obj, would_skip, options):
+    if name == "__init__":
+        return False
+    return would_skip
+
+
+def setup(app):
+    app.connect("autodoc-skip-member", skip)
